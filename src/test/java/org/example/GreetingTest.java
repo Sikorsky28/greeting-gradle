261292b5880c37f07931169583c973fad17891ac
Max Sikorsky <pain.maker28@gmail.com>
2025-04-15 14:08:16 +0100

    MP-10 выполнили задание

--- src/test/java/org/example/GreetingTest.java
+++ src/test/java/org/example/GreetingTest.java
@@ -1,4 +1,16 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class GreetingTest {
-  
+    @Test
+    void shouldGreeting(){
+        Greeting greeting = new Greeting();
+        String result = greeting.greeting();
+        assertEquals("Hello MP-10 done",result);
+
+    }
+
 }
